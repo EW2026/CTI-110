17fc8e01b9e0b3aee3302d8512365082dcc8ec6f
EW2026 <evan.westcott10@gmail.com>
2026-01-29 15:35:42 +0000

    Added variable for name and print hello name

--- hello_students.py
+++ hello_students.py
@@ -1,3 +1,11 @@
 # Printing Hello Everyone. Whats New?🤦‍♂️
 print()
 print("Hello Everyone. Whats New?🤦‍♂️")
+
+# Create variable to hold user's name and get their information
+name=input("Enter Your Name: ")
+
+# Say Hi to User
+print()
+print("Hello my friend,", name)
+print()
