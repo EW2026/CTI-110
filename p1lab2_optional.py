78c016e43eb49b7305b481498514098d0b1ce738
EW2026 <evan.westcott10@gmail.com>
2026-02-05 15:54:51 +0000

    added loops and printing

--- p1lab2_optional.py
+++ p1lab2_optional.py
@@ -3,8 +3,35 @@
 # 20260205
 # Practice with Python and Team Instruction
 
-#User input with message
-name = input("Please enter your first name")
-print()
-print(f"Nice to meet you {name}!!!")
-print()
+
+# name = Bob
+
+# Forced to enter first name
+fname = input("Please enter your first name: ")
+
+while fname == "":
+    print("Please enter your first name ")
+    fname = input("Please enter your first name ")
+lname = input("Please enter your last name ")
+
+# Forced to enter last name
+while lname == "":
+    print("Please enter your last name")
+    fname = input("Please enter your last name ")
+
+hobby = input("what do you like to do for fun? ")
+while hobby == "":
+    print("Please enter your hobby")
+    hobby = input("what do you like to do for fun? ")
+
+
+# If first name and last name are entered, then print message
+print() # Extra Blank Line
+print(f"Nice to meet you {fname} {lname}!!!")
+print(f"I'm glad you like to do {hobby}.")
+print() # Extra Blank Line
+
+print("I can repeat myself!")
+count = int(input("How many times should I repeat myself? "))
+for time in range(count):
+    print("I know a song that drives people crazy,")
