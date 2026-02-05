bfdfcbc33805771471b28c6b90b9ee20a9653b7a
EW2026 <evan.westcott10@gmail.com>
2026-02-05 16:47:24 +0000

    added additional verification steps

--- p1lab2_optional.py
+++ p1lab2_optional.py
@@ -8,20 +8,18 @@
 
 # Forced to enter first name
 fname = input("Please enter your first name: ")
-while fname == "":
-    print("Please enter your first name ")
-    fname = input("Please enter your first name: ")
+while fname == "" or not fname.isalpha():
+    fname = input("Name Entry Invalid Please reenter your first name: ")
+
 
 # Forced to enter last name
 lname = input("Please enter your last name: ")
-while lname == "":
-    print("Please enter last name ")
-    lname = input("Please enter your last name: ")
+while lname == "" or not fname.isalpha():
+    lname = input("Name Entry Invalid Please reenter your last name: ")
 
 hobby = input("what do you like to do for fun?: ")
-while hobby == "":
-    print("Please enter your hobby")
-    hobby = input("what do you like to do for fun?: ")
+while hobby == "" or not fname.isalpha():
+    hobby = input("Hobby Entry Invalid Please reenter your Hobby: ")
 
 
 # If first name and last name are entered, then print message
@@ -33,7 +31,7 @@ print() # Extra Blank Line
 # Introduction to for loops
 print("I can repeat myself!")
 count = input("How many times should I repeat myself?: ")
-while count == "":
+while count == "" or not count.isdigit():
     print("please enter count")
     count = input("How many times should I repeat myself?: ")
 # Valid in put was given
