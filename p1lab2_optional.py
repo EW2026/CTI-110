519320fa02840e8ae60b2cd36fa60e56d0327bb1
EW2026 <evan.westcott10@gmail.com>
2026-02-05 15:36:34 +0000

    Started This Program today

---
+++ p1lab2_optional.py
@@ -0,0 +1,10 @@
+# CTI-110
+# Westcott, Evan
+# 20260205
+# Practice with Python and Team Instruction
+
+#User input with message
+name = input("Please enter your first name")
+print()
+print(f"Nice to meet you {name}!!!")
+print()
