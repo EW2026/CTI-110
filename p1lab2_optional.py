89c75adcf68d6d22e79c7ddaa833e9a4f0c5cf76
EW2026 <evan.westcott10@gmail.com>
2026-02-05 16:09:12 +0000

    Fixed some errors

--- p1lab2_optional.py
+++ p1lab2_optional.py
@@ -8,21 +8,20 @@
 
 # Forced to enter first name
 fname = input("Please enter your first name: ")
-
 while fname == "":
     print("Please enter your first name ")
-    fname = input("Please enter your first name ")
-lname = input("Please enter your last name ")
+    fname = input("Please enter your first name: ")
 
 # Forced to enter last name
+lname = input("Please enter your last name: ")
 while lname == "":
-    print("Please enter your last name")
-    fname = input("Please enter your last name ")
+    print("Please enter last name ")
+    lname = input("Please enter your last name: ")
 
-hobby = input("what do you like to do for fun? ")
+hobby = input("what do you like to do for fun?: ")
 while hobby == "":
     print("Please enter your hobby")
-    hobby = input("what do you like to do for fun? ")
+    hobby = input("what do you like to do for fun?: ")
 
 
 # If first name and last name are entered, then print message
@@ -33,9 +32,9 @@ print() # Extra Blank Line
 
 # Introduction to for loops
 print("I can repeat myself!")
-count = int(input("How many times should I repeat myself? "))
+count = int(input("How many times should I repeat myself?: "))
 while count == "":
     print("please enter count")
-    count = int(input("How many times should I repeat myself? "))
+count = int(input("How many times should I repeat myself?: "))
 for time in range(count):
     print("I know a song that drives people crazy,")
