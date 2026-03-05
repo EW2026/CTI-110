d7ac657c3d5a05913e156dfc71acf30e155e41be
EW2026 <evan.westcott10@gmail.com>
2026-03-05 12:12:16 -0500

    Add files via upload

--- p3hw2_westcott_evan.py
+++ p3hw2_westcott_evan.py
@@ -3,18 +3,28 @@
 # P3HW2
 # Algorithm Tracks Pay Checks
 
-################# Class #################
+
+
+
+
+
+
+#################### FUNCTIONS #######################
 
 def ot(employee_hours, employee_pay, TIME_HALF):
     if employee_hours > 40:
         overtime = employee_hours - 40
-        overtime_pay = employee_pay * TIME_HALF
+        OVERTIME_PAY = employee_pay * TIME_HALF
+        overtime_pay = OVERTIME_PAY * overtime
     else:
         overtime = 0
         overtime_pay = 0
-    return overtime, overtime_pay 
+    return overtime, overtime_pay
     
-def gross_pay(overtime, overtime_pay, time_half):
+
+
+    
+def GROSS_PAY(overtime, overtime_pay, time_half):
     gross_pay = employee_hours * employee_pay
     
     if employee_hours <= 40:
@@ -31,17 +41,19 @@ def gross_pay(overtime, overtime_pay, time_half):
         gross_pay = overtime * overtime_pay + gross_pay
     return gross_pay
 
+
+
+
 def time_and_half(employee_pay):
     HOLIDAY = []
-    days= []
     days = int(input("How many holidays did you work?: "))
     for _ in range(days):
         print("For the question below, please enter all of the following that apply:")
         print("New Years Day, Martin Luther King Jr. Day, Presidents' Day, Memorial Day, Independence Day")
         print("Labor Day, Columbus Day, Veterans Day, Thanksgiving Day, Christmas Day")
-        holiday = holiday(input("What holidays did you work?: "))
+        holiday = input("What holidays did you work?: ")
         while holiday not in ["New Years Day",
-                           "Martin Luther King Jr. Day",
+                            "Martin Luther King Jr. Day",
                             "Presidents' Day",
                             "Memorial Day", 
                             "Independence Day",
@@ -51,27 +63,53 @@ def time_and_half(employee_pay):
                             "Thanksgiving Day",
                             "Christmas Day"]:
             holiday = input("invalid entry(s), please enter the holidays as listed above")
-        holiday = HOLIDAY.append(holiday)   
-        time_half = len(HOLIDAY) * holiday_hours
-        time_half = time_half * TIME_HALF
-        time_half = time_half * employee_pay
+        HOLIDAY.append(holiday)   
+    time_half = len(HOLIDAY) * holiday_hours
+    time_half = time_half * TIME_HALF
+    time_half = time_half * employee_pay
     return time_half 
 
-def list_length(employee_name, employee_hours, employee_pay):
-    employee_list = {employee_name: {}}
+
+
+
+def list_length(employee_name, employee_hours, employee_pay, overtime, overtime_pay, holiday_hours, time_half, gross_pay, EMPLOYEE_NAME, EMPLOYEE_INFO):
+    # EMPLOYEE_NAME = {"name": employee_name, "pay": EMPLOYEE_INFO}
+    #EMPLOYEE_INFO[EMPLOYEE_NAME] = {"Pay": employee_pay, "Hours": employee_hours, "O/T": overtime, "O/T Pay": overtime_pay, "Holiday Hours": holiday_hours, "Holiday PAY": time_half, "Gross Pay": gross_pay} 
+    
     for _ in range(num_employees):
-        employee_hours
-        employee_pay
+        EMPLOYEE_INFO[employee_name] = {"Pay": employee_pay, "Hours": employee_hours, "O/T": overtime, "O/T Pay": overtime_pay, "Holiday Hours": holiday_hours, "Holiday PAY": time_half, "Gross Pay": gross_pay} 
+    return EMPLOYEE_INFO
 
 
 
-    ################# Variables ##############
 
 
+# ################# Variables ##############
+
+
+# Call everything
 num_employees = int(input("Enter number of employees for current pay period: "))
-employee_name = input("Enter employee name (Last, First): ")
-employee_hours = float(input("Enter Employee Hours: "))
-employee_pay = float(input("Enter Employee Pay: "))
-holiday_hours = float(input("How many hours did you work for the holiday(s) you worked?: "))
 
 TIME_HALF = 1.5
+EMPLOYEE_NAME = {}
+EMPLOYEE_INFO = {}
+
+for _ in range(num_employees):
+    employee_name = input("Enter employee name (Last, First): ")
+    employee_hours = float(input("Enter Employee Hours: "))
+    employee_pay = float(input("Enter Employee Pay: "))
+    holiday_hours = float(input("How many hours did you work for the holiday(s) you worked?: "))
+    
+    
+    overtime, overtime_pay = ot(employee_hours,employee_pay, TIME_HALF)
+    
+    time_half = time_and_half(employee_pay)
+    
+    print(time_half)
+    
+    gross_pay = GROSS_PAY(overtime, overtime_pay, time_half)
+    
+    # Call the function that populates the dictionary
+    main_dict = list_length(employee_name, employee_hours, employee_pay, overtime, overtime_pay, holiday_hours, time_half, gross_pay, EMPLOYEE_NAME, EMPLOYEE_INFO)
+    
+    print(main_dict)
